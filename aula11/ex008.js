7024106358433c7cd36094081cc23a7faed65ad0
Danilo Freire <danfreire.dev@gmail.com>
2025-07-03 09:45:47 -0300

    Criei o ex008

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 75.7
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+if (vel > 60) {
+    console.log(`VocÊ ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
